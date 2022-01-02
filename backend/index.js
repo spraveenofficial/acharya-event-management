35d8fffc612c1f3581d5e2635f420853cd429017
Praveen <spraveen593@gmail.com>
2022-01-02 13:41:36 +0530

    Added 4001

--- backend/index.js
+++ backend/index.js
@@ -32,6 +32,6 @@ app.use("/event", (req, res) => {
   });
 });
 
-app.listen(443, (req, res) => {
-  console.log(`Listening on port 443`);
+app.listen(PORT, (req, res) => {
+  console.log(`Listening on port ${PORT}`);
 });
