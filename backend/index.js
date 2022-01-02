3aafef74e7b4899675103c5257961e7ab2cc6ad4
Praveen <spraveen593@gmail.com>
2022-01-02 14:40:33 +0530

    stucked in cors

--- backend/index.js
+++ backend/index.js
@@ -12,10 +12,7 @@ app.use(express.json());
 app.use("/api", require("./routes/routes"));
 app.use(
   cors({
-    origin: "*",
-    methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
-    preflightContinue: false,
-    optionsSuccessStatus: 204,
+    origin: "https://main.d2xlx4fu5imths.amplifyapp.com",
     credentials: true,
   })
 );
