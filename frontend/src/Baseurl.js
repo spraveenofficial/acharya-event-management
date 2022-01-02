a7ab6cc683f4f2c891adc13f5a733fbd61efc2a1
Praveen <spraveen593@gmail.com>
2022-01-02 14:02:36 +0530

    added backend working api

--- frontend/src/Baseurl.js
+++ frontend/src/Baseurl.js
@@ -2,5 +2,5 @@ require("dotenv").config();
 
 export const baseUrl =
   process.env.REACT_APP_NODE_ENV === "development"
-    ? "http://3.145.180.84:4001/api"
+    ? "https://songslyrics.me/api"
     : "http://localhost:4001/api/";
